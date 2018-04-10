7c9b2e8b61c28aa6c85cef9b668cd15ba471b6cf
schandreas <andreas@restental.at>
2018-04-10 19:47:52 +0200

    Added *some* GUI. not at all finished
    come to think of it, nothing is finished rn :thinking:

--- src/at/andreas/restental/Tasker/TaskerMain.java
+++ src/at/andreas/restental/Tasker/TaskerMain.java
@@ -1,6 +1,7 @@
 package at.andreas.restental.Tasker;
 
 
+import at.andreas.restental.Tasker.gui.TaskerGUI;
 import at.andreas.restental.Tasker.io.TaskerXMLParser;
 
 public class TaskerMain {
@@ -8,6 +9,7 @@ public class TaskerMain {
     public static void main(String args[]){
         Task tsk[] = new TaskerXMLParser(args[0]).readTasks();
         System.out.println("got " + tsk[0].getName());
+        new TaskerGUI();
     }
 
 
--- src/at/andreas/restental/Tasker/gui/TaskerGUI.java
+++ src/at/andreas/restental/Tasker/gui/TaskerGUI.java
@@ -1,8 +1,35 @@
 package at.andreas.restental.Tasker.gui;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 public class TaskerGUI extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5997990811036948155L;
+
+	public TaskerGUI() {
+		super("Tasker");
+		this.setSize(400, 400);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Task List");
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("New Task");
+		panel.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		this.setVisible(true);
+	}
 
 
 
