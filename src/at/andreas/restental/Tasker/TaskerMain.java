7e6e4dbd1d5b78818890a77aee679e6090492d55
schandreas <andreas@restental.at>
2018-04-11 10:42:08 +0200

    Removed unnecessary println
    begone :snake:

--- src/at/andreas/restental/Tasker/TaskerMain.java
+++ src/at/andreas/restental/Tasker/TaskerMain.java
@@ -8,7 +8,6 @@ public class TaskerMain {
 
     public static void main(String args[]){
         Task tsk[] = new TaskerXMLParser(args[0]).readTasks();
-        System.out.println("got " + tsk[0].getName());
         TaskerGUI test = new TaskerGUI();
         for(int i = 0; i < tsk.length; i++) {
         	test.addGUITask(tsk[i]);
